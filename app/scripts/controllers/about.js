9d8ab2be788eb220f658840c644f079fb2c2eb49
Justin Wallace <ness839@gmail.com>
2014-07-01 11:50:30 -0400

    Project begin

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name nflplayersApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the nflplayersApp
+ */
+angular.module('nflplayersApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name nflplayersApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the nflplayersApp
+ */
+angular.module('nflplayersApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
