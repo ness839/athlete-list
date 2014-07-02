0af3f7a839e25d10e0ded718cc9412ff6a052594
Justin Wallace <ness839@gmail.com>
2014-07-02 13:38:38 -0400

    Added functionality for buttons

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,9 +9,23 @@
  */
 angular.module('nflplayersApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
+  	
+    $scope.sportList = [
+      {'name': 'NFL',
+       'snippet': 'National Football League',
+       'id': 1 },
+      {'name': 'MLB',
+       'snippet': 'Major League Baseball',
+       'id': 2},
+      {'name': 'NBA',
+       'snippet':'National Basketball Association',
+       'id': 3}
     ];
+    
+    $scope.sport = {};
+    
+    $scope.setSport = function(chosen) {
+    	$scope.sport = $scope.sportList[chosen - 1];
+    };
+    
   });
