9976f711790a67e4ad45bba440e2e4955a418480
Justin Wallace <ness839@gmail.com>
2014-07-02 14:41:58 -0400

    Added API call

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,7 +8,7 @@
  * Controller of the nflplayersApp
  */
 angular.module('nflplayersApp')
-  .controller('MainCtrl', function ($scope) {
+  .controller('MainCtrl', function ($scope, $http) {
   	
     $scope.sportList = [
       {'name': 'NFL',
@@ -23,9 +23,18 @@ angular.module('nflplayersApp')
     ];
     
     $scope.sport = {};
+    $scope.players = {};
     
     $scope.setSport = function(chosen) {
     	$scope.sport = $scope.sportList[chosen - 1];
     };
+    
+    $http.get('http://api.espn.com/v1/sports/football/nfl/athletes/?apikey=ehtaq9zm53tbchrtg7tta6e3')
+    	.success(function(data){
+    		$scope.players = data;
+    		window.alert('It worked!');
+    		console.log($scope.players);
+    	});
+    
     
   });
